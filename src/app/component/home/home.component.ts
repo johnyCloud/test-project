fe885ea1490f58695604b87497debae50ef7002e
John Cloud <ion.nourgs@gmail.com>
2021-09-09 19:39:30 +0300

    finaly done

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -11,51 +11,91 @@ import { SearchLocation } from '../search-form/model';
 })
 export class HomeComponent implements OnInit {
 
-  // elements: any = null;
-  // headElements = [ 'Carrier Name', 'Email', 'Phone Number'];
-  public citiesList : any = [];
+  public elements: any = null;
+  public headElements = [ 'Carrier Name', 'Email', 'Phone Number'];
 
+  public citiesList : any[] = [];
   public location: any;
+  public locationList: any[] = [];
  
   constructor(public httpService : HttpService) { }
 
   ngOnInit(): void {
-    this.httpService.getCities()
-    .subscribe(
-      res =>{
-        this.citiesList = res
-      }
-    )
-    console.log(this.citiesList);
-    
+    this.getCitiesList();
   }
 
+  //get selctet location from form
   setLocation(newLocation : SearchLocation){
     this.location = newLocation;
     console.log(this.location);
+    this.getCitiesInRadius(this.location);
+    this.getData(this.locationList)
   }
 
+  //cities location coordonates
+  getCitiesList(){
+    this.httpService.getCities()
+    .subscribe(res =>
+      {this.citiesList = Object.values(res)}
+    )
+  }
 
+//get all the cities in a given radius
+  getCitiesInRadius(location: SearchLocation){
+    let lt : number;
+    let ln : number;
+    this.citiesList.forEach(item => {
+      if(item.city === location.city) 
+        {
+          lt = item.latitude;
+          ln = item.longitude;
+        }
+    })
+    this.citiesList.forEach(item => {
+      if(location.radius > this.getDistance(lt, ln, item.latitude, item.longitude)) 
+        {
+          this.locationList.push(item.city)
+        }
+    })
+  }
+
+  //calculate distance between 2 locations
+  getDistance(lat1: any, lon1: any, lat2: any, lon2: any){
+    const R = 6371e3; // metres
+    const φ1 = lat1 * Math.PI/180; // φ, λ in radians
+    const φ2 = lat2 * Math.PI/180;
+    const Δφ = (lat2-lat1) * Math.PI/180;
+    const Δλ = (lon2-lon1) * Math.PI/180;
+ 
+    const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+      Math.cos(φ1) * Math.cos(φ2) *
+      Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+ 
+    const d = R * c; // in metres
+    return d/1000;
+  }
   
 
-  //executet on search button
-  // getData(data : any){
+  //get list of carriers
+  getData(data : any){
     
-  //   let address = (data.city + " " + data.state);
+    let address = data;
+    console.log(address);
     
-  //   this.requestsService.postReq(address)
-  //       .subscribe(
-  //          res => {
-  //           this.elements = [];
-  //           let values = Object.values(res);
-  //           values.forEach(value => {
-  //             this.elements.push({
-  //               'name': value.Name, 
-  //               'email': value.Address.Email[0], 
-  //               'phone': value.Address.Phone[0]});     
-  //           })
-  //       }
-  //     )
-  // }
+    this.httpService.postReq(address)
+        .subscribe(
+           res => {
+            this.elements = [];
+            let values = Object.values(res);
+            values.forEach(value => {
+              this.elements.push({
+                'name': value.Name, 
+                'email': value.Address.Email[0], 
+                'phone': value.Address.Phone[0]});     
+            })
+        }
+      )
+  }
   
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,10 +1,8 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { APIRsponse } from './model';
-//import * as states from './assets/states.json'
-
-
+import { environment as env } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +20,16 @@ export class HttpService {
   getCities() : Observable<any>{
     let url = '../../assets/cities.json';
     return this.http.get<any>(url)
+  }
+
+  //get by post request all carriers by given locations
+  postReq(adress : string) : Observable<APIRsponse> { 
+    let params =  new HttpParams()
+      .set("Addresses", adress)
+    let headers = new HttpHeaders()
+      .set('Authorization', 'BasicamRldjpUbU5zTFRBeFl6Y3lOREkwTFRsak1HSXROR1EwWmkxaVpqYzFMV1JrT1RjM09XWXdZakJqTWkwdGJYSks=');
+
+    return this.http.post<APIRsponse>(env.BASE_URL, params, {'headers' : headers});
   }
   
 }
